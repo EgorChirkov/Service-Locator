bd5b0d2ee5da1773d42495785937a37717f8f26a
Egor <egorchirkov03@gmail.com>
2022-11-04 15:57:25 +0300

    Initial Commit

---
+++ Service Locator/Service_LocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Service_LocatorApp.swift
+//  Service Locator
+//
+//  Created by Егор Чирков on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Service_LocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
